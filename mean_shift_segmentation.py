dac3929423702b08d47d27b9b8aed99cd0faf21c
Seekerzero <33109925+Seekerzero@users.noreply.github.com>
2021-12-03 16:50:13 -0500

    update scikit_clustering method

--- mean_shift_segmentation.py
+++ mean_shift_segmentation.py
@@ -5,14 +5,14 @@ original_image = cv2.imread("example-orig.jpg")
 
 bgrimg = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY) 
 
-(segmented_image, labels_image, number_regions) = pms.segment(bgrimg, spatial_radius=6, 
-                                                              range_radius=4.5, min_density=50)
+(segmented_image, labels_image, number_regions) = pms.segment(bgrimg, spatial_radius=10, 
+                                                              range_radius=10, min_density=300)
 
 # img = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY)                                                         
 
 ret, thresh = cv2.threshold(segmented_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-connectivity = 4 
+connectivity = 8 
 
 output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
 
---
+++ scikit-learn_mean_shift_segmentation.py
@@ -0,0 +1,41 @@
+from sklearn.cluster import MeanShift, estimate_bandwidth
+import numpy as np
+import cv2
+
+original_image = cv2.imread("right_img.png")
+blurimg = cv2.medianBlur(original_image, 3)
+# bgrimg = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+flat_image = blurimg.reshape((-1,3))
+flat_image = np.float32(flat_image) 
+bandwidth = estimate_bandwidth(flat_image, quantile=.06, n_samples=3000)
+ms = MeanShift(bandwidth=bandwidth, max_iter=800, bin_seeding=True)
+ms.fit(flat_image)
+labeled = ms.labels_
+
+segments = np.unique(labeled)
+print('Number of segments: ', segments.shape[0])
+print("cluster centroids", ms.cluster_centers_)
+print("length of centroid matrix", len(ms.cluster_centers_))
+
+# get the average color of each segment
+total = np.zeros((segments.shape[0], 3), dtype=float)
+count = np.zeros(total.shape, dtype=float)
+for i, label in enumerate(labeled):
+    total[label] = total[label] + flat_image[i]
+    count[label] += 1
+avg = total/count
+avg = np.uint8(avg)
+
+# cast the labeled image into the corresponding average color
+res = avg[labeled]
+result = res.reshape((blurimg.shape))
+
+cv2.imshow('result',result)
+cv2.imwrite("right_image.png",result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# print("cluster labels", clustering.labels_)
+
+# cv2.imwrite('scikit_clustering.png', clustering.labels_)
